f5b3cc189efc8d5399a7cb609c31787aa609ef0b
System Administrator <root@MBP-de-Maicon.home>
2021-06-28 06:59:23 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ButtonHTMLAttributes } from 'react'
+import '../styles/button.scss'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    isOutlined?: boolean
+};
+
+export const Button = ({isOutlined = false, ...props}: ButtonProps) => {
+    return (
+        <button 
+            className={`button ${isOutlined ? 'outlined' : '' }`} 
+            {...props} 
+        />
+    )
+}
